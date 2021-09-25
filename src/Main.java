f017026721fd92faf638270ef2154dec77d0b32c
JacaOsom <jacekfilipski@gmail.com>
2021-09-25 09:20:59 +0200

    Zaczynamy naukę! :)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+        System.out.println("Hello World!");
+        System.out.println("Zaczynamy przygodę z programowanie JAVA!");
+        System.out.println("Good Luck!");
 
     }
 }
