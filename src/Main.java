cd663bc3ca9161ce3b956e5c5caf07a1125f3802
JacaOsom <jacekfilipski@gmail.com>
2021-09-25 10:36:43 +0200

    ćwiczenie 4, kilka operacji arytmetycznych i logicznych

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,17 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        int liczbaIntA = 12, liczbaIntB = 20;
+        double liczbaDoubleA = 25.25, liczbaDoubleB = 8.50;
+        System.out.println(liczbaIntA == 12 && liczbaIntB == 10);  //false
+        System.out.println(liczbaIntA == 11 || liczbaIntB != 10);  //true
+        System.out.println(liczbaIntA >= 12 && liczbaIntB < 100);  //true
+        System.out.println(liczbaIntA < 8 || liczbaIntB == 10);  //false
+        System.out.println((liczbaIntA < 8 | liczbaIntB == 20) && liczbaIntA == 12); //true
+        System.out.println(liczbaIntA == liczbaIntB || liczbaIntB > 10);  //true
+        liczbaIntA = liczbaIntA + (int)liczbaDoubleB;
+        System.out.println(liczbaIntA);
+        System.out.println(liczbaDoubleA + liczbaIntA);
 
     }
 }
