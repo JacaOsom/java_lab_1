2bf8219167e2f07e197650f4266a35e6df5be4e3
JacaOsom <jacekfilipski@gmail.com>
2021-09-25 08:53:32 +0200

    Zaczynamy przygodę!

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        int zmiennaInt = 7;
-        System.out.println("zmienna int = " + zmiennaInt);
-        /* ćw.2 */
-
-        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
-
-        /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
-
-
+        System.out.println("Witaj świecie!");
+        //System.out.println("razdwatrzy");
+        System.out.println("Zaczynamy przygodę z Java!");
 
     }
 }
