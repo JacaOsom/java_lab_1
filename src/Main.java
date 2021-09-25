ed91fa67e4fb78d20bfd02192c0d1e116650a175
JacaOsom <jacekfilipski@gmail.com>
2021-09-25 10:13:54 +0200

    ćwiczenie 3, wczytywanie danych z klawiatury. Let's go!

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,21 @@ public class Main {
         *   dane mają być wprowadzane z klawiatury w konsoli
         *   b) wyświetlić dane za pomocą println i printf
         */
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Podaj imię: ");
+        String imie = scan.next();
+        System.out.print("Podaj Nazwisko: ");
+        String nazwisko = scan.next();
+        System.out.print("Podaj wiek: ");
+        int wiek = scan.nextInt();
+        System.out.print("Podaj numer indeksu: ");
+        int nrIndeksu = scan.nextInt();
+
+        System.out.printf("Imię: %s\nNazwisko: %s\nWiek: %d\nNumer indeksu: %d", imie, nazwisko, wiek, nrIndeksu);
+        System.out.println();
+        System.out.println();
+        System.out.println("Imię i nazwisko: " + imie + " " + nazwisko + " Wiek: " + wiek + " Nr Indeksu: " + nrIndeksu);
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
