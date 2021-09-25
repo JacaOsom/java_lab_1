0dfb303ed9c478a2dc2f8097cb3325d690ede881
JacaOsom <jacekfilipski@gmail.com>
2021-09-25 08:28:33 +0200

    Zaczynamy przygodę!

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Witaj świecie!");
-        System.out.println("razdwatrzy");
+        //System.out.println("razdwatrzy");
+        System.out.println("Zaczynamy przygodę z Java!");
 
     }
 }
